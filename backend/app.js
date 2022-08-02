39f3cac00810da058a2b49297890607621b7fed8
unknown <ignaciorom27@gmail.com>
2022-08-02 14:58:55 -0500

    feat: set mongo connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
+const port =  process.env.PORT || 5000;
+const mongodb = process.env.MONGODB_URL;
 
-app.listen(3000);
-console.log("Server on port: ", 3000);
+app.use(bodyParser.json());
+
+mongoose.connect(mongodb)
+.then(() => {
+    app.listen(port);
+    console.log("Connected to Stori database & Server listening on port: ", port);
+})
+.catch((err)=> {
+    console.error(err)
+})
