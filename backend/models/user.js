49685edcc343f51342e447a566999574481f1c63
unknown <ignaciorom27@gmail.com>
2022-08-02 19:18:20 -0500

    feat: user recepient and newsletter model created

---
+++ backend/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema([{
+    name: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true
+    }
+}]);
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
